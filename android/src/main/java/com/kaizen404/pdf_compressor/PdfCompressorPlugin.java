d33726c28c1ba9261321b365efc7b0f50a0a2e31
vikassandhu999 <vikassandhu999@gmail.com>
2021-05-31 03:39:44 +0530

    image-compression improved

--- android/src/main/java/com/kaizen404/pdf_compressor/PdfCompressorPlugin.java
+++ android/src/main/java/com/kaizen404/pdf_compressor/PdfCompressorPlugin.java
@@ -4,11 +4,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import androidx.annotation.NonNull;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.exceptions.BadPasswordException;
 import com.itextpdf.text.pdf.PRStream;
 import com.itextpdf.text.pdf.PdfName;
 import com.itextpdf.text.pdf.PdfNumber;
@@ -16,18 +12,13 @@ import com.itextpdf.text.pdf.PdfObject;
 import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.PdfStamper;
 import com.itextpdf.text.pdf.parser.PdfImageObject;
-import com.itextpdf.text.xml.xmp.PdfSchema;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
 
 /** PdfCompressorPlugin */
 public class PdfCompressorPlugin implements FlutterPlugin, MethodCallHandler {
@@ -35,14 +26,8 @@ public class PdfCompressorPlugin implements FlutterPlugin, MethodCallHandler {
   private MethodChannel channel;
 
   @Override
-  public void onAttachedToEngine(
-    @NonNull FlutterPluginBinding flutterPluginBinding
-  ) {
-    channel =
-      new MethodChannel(
-        flutterPluginBinding.getBinaryMessenger(),
-        "pdf_compressor"
-      );
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "pdf_compressor");
     channel.setMethodCallHandler(this);
   }
 
@@ -93,18 +78,13 @@ public class PdfCompressorPlugin implements FlutterPlugin, MethodCallHandler {
       reader.close();
     }
 
-    private void saveCompressedPdfFromReader(PdfReader reader)
-      throws Exception {
-      PdfStamper stamper = new PdfStamper(
-        reader,
-        new FileOutputStream(this.outputPath)
-      );
+    private void saveCompressedPdfFromReader(PdfReader reader) throws Exception {
+      PdfStamper stamper = new PdfStamper(reader, new FileOutputStream(this.outputPath));
       stamper.setFullCompression();
       stamper.close();
     }
 
-    private void optimizeAllXrefObjectsFromReader(PdfReader reader)
-      throws Exception {
+    private void optimizeAllXrefObjectsFromReader(PdfReader reader) throws Exception {
       int xrefSize = reader.getXrefSize();
       for (int iter = 1; iter <= xrefSize; iter++) {
         PdfObject pdfObject = reader.getPdfObject(iter);
@@ -116,62 +96,61 @@ public class PdfCompressorPlugin implements FlutterPlugin, MethodCallHandler {
         PRStream pRStream = (PRStream) pdfObject;
 
         if (subtypeIsImage(pRStream)) {
-          compressXrefImageFromPRStream(pRStream);
-          continue;
+          compressXrefImageFromPRStream2(pRStream);
         }
       }
     }
 
-    private void compressXrefImageFromPRStream(PRStream pRStream)
-      throws Exception {
+    /**
+     * 
+     * @param pRStream
+     * @throws Exception
+     */
+    private void compressXrefImageFromPRStream(PRStream pRStream) throws Exception {
       byte[] imageAsBytes = new PdfImageObject(pRStream).getImageAsBytes();
-      Bitmap imageBitmap = BitmapFactory.decodeByteArray(
-        imageAsBytes,
-        0,
-        imageAsBytes.length
-      );
+      Bitmap imageBitmap = BitmapFactory.decodeByteArray(imageAsBytes, 0, imageAsBytes.length);
 
       if (imageBitmap != null) {
         int width = imageBitmap.getWidth();
         int height = imageBitmap.getHeight();
 
-        Bitmap outputImageBitmap = Bitmap.createBitmap(
-          width,
-          height,
-          Bitmap.Config.ARGB_8888
-        );
-        new Canvas(outputImageBitmap)
-        .drawBitmap(imageBitmap, 0.0f, 0.0f, (Paint) null);
+        Bitmap outputImageBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        new Canvas(outputImageBitmap).drawBitmap(imageBitmap, 0.0f, 0.0f, (Paint) null);
         if (!imageBitmap.isRecycled()) {
           imageBitmap.recycle();
         }
 
         ByteArrayOutputStream outputImageStream = new ByteArrayOutputStream();
-        outputImageBitmap.compress(
-          Bitmap.CompressFormat.JPEG,
-          this.quality,
-          outputImageStream
-        );
+        outputImageBitmap.compress(Bitmap.CompressFormat.JPEG, this.quality, outputImageStream);
         if (!outputImageBitmap.isRecycled()) {
           outputImageBitmap.recycle();
         }
 
-        resetPRStreamForImage(
-          pRStream,
-          outputImageStream.toByteArray(),
-          width,
-          height
-        );
+        resetPRStreamForImage(pRStream, outputImageStream.toByteArray(), width, height);
         outputImageStream.close();
       }
     }
 
-    private void resetPRStreamForImage(
-      PRStream stream,
-      byte[] data,
-      int width,
-      int height
-    ) {
+    /**
+     * 
+     * @param pRStream
+     * @throws Exception
+     */
+    private void compressXrefImageFromPRStream2(PRStream pRStream) throws Exception {
+      byte[] imageAsBytes = new PdfImageObject(pRStream).getImageAsBytes();
+      ByteArrayOutputStream outputImageStream = new ByteArrayOutputStream();
+
+      compressImage(imageAsBytes, quality, outputImageStream);
+
+      if (!outputImageBitmap.isRecycled()) {
+        outputImageBitmap.recycle();
+      }
+
+      resetPRStreamForImage(pRStream, outputImageStream.toByteArray(), width, height);
+      outputImageStream.close();
+    }
+
+    private void resetPRStreamForImage(PRStream stream, byte[] data, int width, int height) {
       stream.clear();
       stream.setData(data, false, this.compressionLevel);
       stream.put(PdfName.TYPE, PdfName.XOBJECT);
@@ -185,13 +164,113 @@ public class PdfCompressorPlugin implements FlutterPlugin, MethodCallHandler {
 
     private boolean subtypeIsImage(PRStream stream) {
       PdfObject object = stream.get(PdfName.SUBTYPE);
-      return (
-        object != null && object.toString().equals(PdfName.IMAGE.toString())
-      );
+      return (object != null && object.toString().equals(PdfName.IMAGE.toString()));
     }
 
     private boolean objectIsStream(PdfObject object) {
       return (object != null && object.isStream());
     }
+
+    /**
+     * 
+     * @param imageAsBytes
+     * @param quality
+     * @param outputImageStream
+     * @throws Exception
+     */
+    private void compressImage(byte[] imageAsBytes, int quality, ByteArrayOutputStream outputImageStream)
+        throws Exception {
+      Bitmap scaledBitmap = null;
+
+      BitmapFactory.Options options = new BitmapFactory.Options();
+
+      // by setting this field as true, the actual bitmap pixels are not loaded in the
+      // memory. Just the bounds are loaded. If
+      // you try the use the bitmap here, you will get null.
+      options.inJustDecodeBounds = true;
+      Bitmap bmp = BitmapFactory.decodeByteArray(imageAsBytes, 0, imageAsBytes.length, options);
+
+      int actualHeight = options.outHeight;
+      int actualWidth = options.outWidth;
+
+      // max Height and width values of the compressed image is taken as 816x612
+      float maxHeight = 816.0f;
+      float maxWidth = 612.0f;
+      float imgRatio = actualWidth / actualHeight;
+      float maxRatio = maxWidth / maxHeight;
+
+      // width and height values are set maintaining the aspect ratio of the image
+
+      if (actualHeight > maxHeight || actualWidth > maxWidth) {
+        if (imgRatio < maxRatio) {
+          imgRatio = maxHeight / actualHeight;
+          actualWidth = (int) (imgRatio * actualWidth);
+          actualHeight = (int) maxHeight;
+        } else if (imgRatio > maxRatio) {
+          imgRatio = maxWidth / actualWidth;
+          actualHeight = (int) (imgRatio * actualHeight);
+          actualWidth = (int) maxWidth;
+        } else {
+          actualHeight = (int) maxHeight;
+          actualWidth = (int) maxWidth;
+
+        }
+      }
+
+      // setting inSampleSize value allows to load a scaled down version of the
+      // original image
+      options.inSampleSize = calculateInSampleSize(options, actualWidth, actualHeight);
+
+      // inJustDecodeBounds set to false to load the actual bitmap
+      options.inJustDecodeBounds = false;
+
+      // this options allow android to claim the bitmap memory if it runs low on
+      // memory
+      options.inPurgeable = true;
+      options.inInputShareable = true;
+      options.inTempStorage = new byte[16 * 1024];
+
+      // load the bitmap from its path
+      bmp = BitmapFactory.decodeByteArray(imageAsBytes, 0, imageAsBytes.length, options);
+      scaledBitmap = Bitmap.createBitmap(actualWidth, actualHeight, Bitmap.Config.ARGB_8888);
+
+      float ratioX = actualWidth / (float) options.outWidth;
+      float ratioY = actualHeight / (float) options.outHeight;
+      float middleX = actualWidth / 2.0f;
+      float middleY = actualHeight / 2.0f;
+
+      Matrix scaleMatrix = new Matrix();
+      scaleMatrix.setScale(ratioX, ratioY, middleX, middleY);
+
+      Canvas canvas = new Canvas(scaledBitmap);
+      canvas.setMatrix(scaleMatrix);
+      canvas.drawBitmap(bmp, middleX - bmp.getWidth() / 2, middleY - bmp.getHeight() / 2,
+          new Paint(Paint.FILTER_BITMAP_FLAG));
+
+      // scaledBitmap = Bitmap.createBitmap(scaledBitmap, 0, 0, scaledBitmap.getWidth(), scaledBitmap.getHeight(), matrix,
+      //     true);
+
+      scaledBitmap.compress(Bitmap.CompressFormat.JPEG, quality, outputImageStream);
+    }
+
+    private int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+      final int height = options.outHeight;
+      final int width = options.outWidth;
+      int inSampleSize = 1;
+
+      if (height > reqHeight || width > reqWidth) {
+        final int heightRatio = Math.round((float) height / (float) reqHeight);
+        final int widthRatio = Math.round((float) width / (float) reqWidth);
+        inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+      }
+      final float totalPixels = width * height;
+      final float totalReqPixelsCap = reqWidth * reqHeight * 2;
+      while (totalPixels / (inSampleSize * inSampleSize) > totalReqPixelsCap) {
+        inSampleSize++;
+      }
+
+      return inSampleSize;
+    }
+
   }
 }
